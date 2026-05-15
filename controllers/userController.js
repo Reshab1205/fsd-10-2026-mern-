1e41c795f5f8ba9fbfc63b2e4eb8449ef97f925a
Reshab Kumar <rkmodi12051998@gmail.com>
2026-05-15 17:16:16 +0530

    reg login

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,26 +4,41 @@ const user = require("../models/userModel");
 const register = async (req, res) => {
   try {
     const inputData = req.body;
+    if(Object.keys(inputData).length === 0) {
+      return res.status(404).json({message: 'Provide Details to Register'})
+    }
     const emailExist = await user.findOne({ email: inputData.email });
     const mobileExist = await user.findOne({ email: inputData.mobile_number });
     const aadharExist = await user.findOne({ email: inputData.aadhar_number });
 
     if (emailExist || mobileExist || aadharExist) {
-      return res.status(404).json({ message: "Email Already Exists" });
+      return res.status(404).json({ message: "Account Already Exists" });
     }
     const data = await user.create(inputData);
     // console.log(emailExist)
 
     return res.status(200).json({ message: "Account Created Successfully", data:data  });
   } catch (err) {
-    console.log(err);
+    return res.status(500).json({message: 'Internal Server Error'})
   }
 };
 
 const login = async (req, res) => {
-  try {
+ try {
+    const inputData = req.body;
+    if(Object.keys(inputData).length === 0) {
+      return res.status(404).json({message: 'Provide Details to Login'})
+    }
+    const emailExist = await user.findOne({ email: inputData.email });
+    if (emailExist === null) {
+      return res.status(404).json({ message: "Account Does not Exists" });
+    }
+    if(!(emailExist.password === inputData.password)) {
+      return res.status(404).json({ message: "Wrong Credentials" });
+    }
+    return res.status(200).json({ message: "Login Successfully"});
   } catch (err) {
-    console.log(err);
+    return res.status(500).json({message: 'Internal Server Error'})
   }
 };
 
--- models/userModel.js
+++ models/userModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     name:{type:String},
     email:{type:String, required:true, unique:true},
+    password:{type:String, required:true},
     mobile_number:{type:String, required:true, unique:true},
     dob:{type:String},
     aadhar_number:{type:Number, unique:true},
